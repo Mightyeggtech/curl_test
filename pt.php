e0a93ace2a4cc8ea1fa24837c914f54f45324011
RifatCse <rifatcse17@gmail.com>
2019-08-25 12:08:09 +0600

    image tested

--- pt.php
+++ pt.php
@@ -12,14 +12,15 @@ use Spatie\Browsershot\Browsershot;
 $br = new Browsershot();
 // $br->setUrl('https://infinite-dawn-72254.herokuapp.com/')
 // ->save('out1.jpg')
+$_POST['rids'] = "546061,546062,546063";
 $link = 'https://infinite-dawn-72254.herokuapp.com/';
 try{
-    $br->setUrl($link)->save('example.pdf');
+    $br->setUrl($link, $_POST)->save('example.jpeg');
 }
 catch (Exception $e) {
     echo $e->getMessage();
 }
-echo "PDF GENERATED!!!";
+echo "IMAGE GENERATED!!!";
 
 
 ?>

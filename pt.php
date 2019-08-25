531a9cc91a40d559697f52dfd0b1b6d9996ae73c
RifatCse <rifatcse17@gmail.com>
2019-08-25 11:28:33 +0600

    pdf tested

--- pt.php
+++ pt.php
@@ -2,18 +2,23 @@
 
 <?php 
 // Turn off all error reporting
-error_reporting(0);
+//error_reporting(0);
 
 require 'vendor/autoload.php';
-require __DIR__ . '/vendor/autoload.php';
+//require __DIR__ . '/vendor/autoload.php';
 use Spatie\Browsershot\Browsershot;
 // $br = new Browsershot();
 // $br->setUrl('https://infinite-dawn-72254.herokuapp.com/')->save('example.pdf');
 $br = new Browsershot();
 // $br->setUrl('https://infinite-dawn-72254.herokuapp.com/')
 // ->save('out1.jpg')
-$link = 'https://stackoverflow.com/questions/7841720/printing-to-printers-in-php';
-$br->setUrl($link)->save('example.pdf');
+$link = 'https://infinite-dawn-72254.herokuapp.com/';
+try{
+    $br->setUrl($link)->save('example.pdf');
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+}
 echo "PDF GENERATED!!!";
 
 

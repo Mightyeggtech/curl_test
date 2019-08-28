4096d6d41c45a8ab18d24774dda06ade87849167
Mightyeggtech <mightyeggtech@gmail.com>
2019-08-28 11:47:27 +0000

    push to digital ocean

--- home/ss-print/file.php
+++ home/ss-print/file.php
@@ -60,12 +60,10 @@ if(isset($_POST['rids'])){
     $rids = $explodedData[$i];
     $counter++;
     $queryString = "{'3'.EX.'$rids'}";
-    echo $queryString;
-    die();
     $url = "https://ss.quickbase.com/db/bprrh3cv9?a=API_DoQuery&query=(".$queryString.")&clist=6.43&usertoken=b3jy7r_mpaj_bnntwqddvusut3bdhsfwwdb2vzx2";
     $cURL = curl_init($url);
     curl_setopt($cURL, CURLOPT_POST, true);
-    curl_setopt($cURL, CURLOPT_POSTFIELDS, '');
+    curl_setopt($cURL, CURLOPT_POSTFIELDS, $_POST);
     curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
     //curl_setopt($cURL, CURLOPT_HTTPHEADER, array("Accept: application/pdf"));
     $response = curl_exec($cURL);
